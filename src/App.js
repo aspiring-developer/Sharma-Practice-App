b946527e3eae3218fc6c6769a58e7cf56009ff74
Shi Sha <emailtotheowner@gmail.com>
2020-07-27 18:10:29 -0400

    External links, supporting modules download complete.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-
 function App() {
   return (
     <div className="container">
-      <h5>App working!</h5>
+      <h1>App working!</h1> <h2>App working!</h2> <h3>App working!</h3> <h4>App working!</h4> <h5>App working!</h5> <h6>App working!</h6>
+      <p>App working!</p>
     </div>
   );
 }
