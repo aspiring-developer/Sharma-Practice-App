6d4027a7cfa8b60c0509b2c5aee16bff5dfea96f
ssh1sharma <emailtotheowner@gmail.com>
2020-12-25 14:34:11 -0500

    Form fields added

--- server.js
+++ server.js
@@ -17,21 +17,31 @@ res.send(`
   <meta charset="utf-8" />
   <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
- <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <link href="styles/reset.css" rel="stylesheet" />
-
+  <link href="styles/mainStyle.css" rel="stylesheet" />
   <title>Sharma Fine Arts</title>
 </head>
 
 <body>
-  <div class="mainWrapper">
+  <div class="container mainWrapper">
   <header>
-  <div class="row">
   <h2>Welcome to my site!!!!!!!!</h2>
-  </div>
   </header>
+  <div class="formWrapper">
+  <form action="/item-list" method="POST">
+<div class="form-group">
+<label for="userInput"> Enter your Name: </label>
+<input type="text" class="for-control" placeholder="your name" name="userInput" />
+<button class="btn btn-info"> Submit </button>
 </div>
+
+  </form>
+  </div>
+</div>
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </body>
 
 </html>`);
